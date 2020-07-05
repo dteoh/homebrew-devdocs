006298bddcab0a83de384cb118a666e2c3520ee1
Douglas Teoh <douglas@dteoh.com>
2020-07-05 20:16:50 +0900

    Release v0.5.2

--- Casks/devdocs-macos.rb
+++ Casks/devdocs-macos.rb
@@ -1,6 +1,6 @@
 cask 'devdocs-macos' do
-  version '0.5.1'
-  sha256 'e7c81d10fc14780e9703ff27d1c02fcb4ba3e2c69900f8e9f47993edc59e05e2'
+  version '0.5.2'
+  sha256 '706986440df2ec1765b483d42876953ebc789cd0170c657cb5266cc945ca25e9'
 
   url "https://github.com/dteoh/devdocs-macos/releases/download/v#{version}/DevDocs.zip"
   name 'DevDocs for macOS'
